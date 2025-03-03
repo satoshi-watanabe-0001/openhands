239f43daa2afdd563d717fee1e9d67ee23511c2c
namunamuyaohachi@gmail.com <namunamuyaohachi@gmail.com>
2025-03-03 06:12:02 +0000

    Update notify_slack.sh with Bot Token and Channel ID

--- notify_slack.sh
+++ notify_slack.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# Slack Incoming Webhook URL (またはBotトークンとチャンネルID) を設定
-read -r -p "Slack Incoming Webhook URLを入力してください (または、'token'と入力してBotトークンとチャンネルIDを入力): " WEBHOOK_URL
+# Slack Bot User OAuth Token と チャンネルID を設定
+***REMOVED***
+CHANNEL_ID="C08GG0WTE9W"
 
-if [[ "$WEBHOOK_URL" == "token" ]]; then
-  read -r -p "Bot User OAuth Tokenを入力してください: " BOT_TOKEN
-  read -r -p "チャンネルIDを入力してください: " CHANNEL_ID
-fi
 
 # 作業の例 (ファイルを作成)
-# ここに実際の作業コマンドを記述
+# ここに実際の作業コマンドを記述。例えば、
+# echo "作業を開始します"
+# ... 何らかの処理 ...
+# echo "作業が完了しました"
 
-# Slackに通知 (Incoming Webhookの場合)
-if [[ "$WEBHOOK_URL" != "token" ]]; then
-  curl -X POST -H 'Content-type: application/json' --data '{"text":"OpenHandsの作業が完了しました！"}' "$WEBHOOK_URL"
-else
-  # Slackに通知 (chat.postMessageの場合)
-  curl -X POST -H 'Authorization: Bearer '$BOT_TOKEN'' \
-      -H 'Content-type: application/json' \
+
+# Slackに通知 (chat.postMessageの場合)
+curl -X POST -H 'Authorization: Bearer '$BOT_TOKEN'' \\
+      -H 'Content-type: application/json' \\
       --data '{
-          "channel": "'$CHANNEL_ID'",
-          "text": "OpenHandsの作業が完了しました！"
-      }' \
+         "channel": "'$CHANNEL_ID'",
+         "text": "OpenHandsの作業が完了しました！"
+      }' \\
       https://slack.com/api/chat.postMessage
-fi
